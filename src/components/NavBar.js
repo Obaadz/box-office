03f531fb1f452cc99c76d7c452ff5c511f307ced
Obada <obada8411@gmail.com>
2022-04-27 01:12:26 +0200

    finishing navbar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
 
 const pages = [
   { id: 1, path: '/', text: 'Home' },
   { id: 2, path: '/starred', text: 'Starred' },
 ];
 
-const pagesList = pages.map(({ id, path, text }) => (
-  <Link key={id} to={path}>
-    {text}
-  </Link>
-));
+const renderPages = currentLocation =>
+  pages.map(({ id, path, text }) => (
+    <li key={id}>
+      <Link to={path} className={path === currentLocation && 'active'}>
+        {text}
+      </Link>
+    </li>
+  ));
+
+const StyledNavBar = styled.div`
+  text-align: center;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+
+    a {
+      position: relative;
+      padding: 5px 10px;
+      text-decoration: none;
+      color: rgb(198, 198, 198);
+      transition: 0.3s all;
+
+      &.active {
+        color: blue;
+      }
+      &.active::before {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        background-color: blue;
+      }
+    }
+  }
+`;
 
 const NavBar = () => {
+  const currentLocation = useLocation();
   return (
-    <div className="navbar">
-      <ul>{pagesList}</ul>
-    </div>
+    <StyledNavBar>
+      <ul>{renderPages(currentLocation.pathname)}</ul>
+    </StyledNavBar>
   );
 };
 
